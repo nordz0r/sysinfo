e7a1910cd1577c7974cd6de0ea3d0b5645576a29
NorD <nordz0r@bitbucket.org>
2019-07-08 13:36:11 +0300

    Add script

---
+++ sysinfo.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+#
+# Description: Sysinfo
+#
+# Copyright (C) 2019 NorD
+#
+# usage: 
+##########################
+
+
+if  [ ! -e '/usr/bin/wget' ]; then
+    echo "Error: wget command not found. You must be install wget command at first."
+    exit 1
+fi
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;36m'
+PLAIN='\033[0m'
+
+get_opsy() {
+    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+
+next() {
+    printf "%-70s\n" "-" | sed 's/\s/-/g'
+}
+
+calc_disk() {
+    local total_size=0
+    local array=$@
+    for size in ${array[@]}
+    do
+        [ "${size}" == "0" ] && size_t=0 || size_t=`echo ${size:0:${#size}-1}`
+        [ "`echo ${size:(-1)}`" == "K" ] && size=0
+        [ "`echo ${size:(-1)}`" == "M" ] && size=$( awk 'BEGIN{printf "%.1f", '$size_t' / 1024}' )
+        [ "`echo ${size:(-1)}`" == "T" ] && size=$( awk 'BEGIN{printf "%.1f", '$size_t' * 1024}' )
+        [ "`echo ${size:(-1)}`" == "G" ] && size=${size_t}
+        total_size=$( awk 'BEGIN{printf "%.1f", '$total_size' + '$size'}' )
+    done
+    echo ${total_size}
+}
+
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F'[ :]' '/cpu MHz/ {print $4;exit}' /proc/cpuinfo )
+tram=$( free -m | awk '/Mem/ {print $2}' )
+uram=$( free -m | awk '/Mem/ {print $3}' )
+swap=$( free -m | awk '/Swap/ {print $2}' )
+uswap=$( free -m | awk '/Swap/ {print $3}' )
+up=$( awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days, %d hour %d min\n",a,b,c)}' /proc/uptime )
+load=$( w | head -1 | awk -F'load average:' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//' )
+opsy=$( get_opsy )
+arch=$( uname -m )
+lbit=$( getconf LONG_BIT )
+kern=$( uname -r )
+ipv6=$( wget -qO- -t1 -T2 ipv6.icanhazip.com )
+disk_size1=($( LANG=C df -hPl | grep -wvE '\-|none|tmpfs|devtmpfs|by-uuid|chroot|Filesystem|udev|docker' | awk '{print $2}' ))
+disk_size2=($( LANG=C df -hPl | grep -wvE '\-|none|tmpfs|devtmpfs|by-uuid|chroot|Filesystem|udev|docker' | awk '{print $3}' ))
+disk_total_size=$( calc_disk "${disk_size1[@]}" )
+disk_used_size=$( calc_disk "${disk_size2[@]}" )
+last_logon=$( who | grep `whoami` | head -1 | awk '{print $3,$4,$5}' )
+ips=$( ip addr show | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}' | egrep -v "127.0.0.1" | xargs )
+
+# clear
+next
+echo -e "CPU model            : ${BLUE}$cname${PLAIN}"
+echo -e "Number of cores      : ${BLUE}$cores${PLAIN}"
+echo -e "CPU frequency        : ${BLUE}$freq MHz${PLAIN}"
+echo -e "Total size of Disk   : ${BLUE}$disk_total_size GB ($disk_used_size GB Used)${PLAIN}"
+echo -e "Total amount of Mem  : ${BLUE}$tram MB ($uram MB Used)${PLAIN}"
+echo -e "Total amount of Swap : ${BLUE}$swap MB ($uswap MB Used)${PLAIN}"
+echo -e "System uptime        : ${BLUE}$up${PLAIN}"
+echo -e "Load average         : ${BLUE}$load${PLAIN}"
+echo -e "OS                   : ${BLUE}$opsy${PLAIN}"
+echo -e "Arch                 : ${BLUE}$arch ($lbit Bit)${PLAIN}"
+echo -e "Kernel               : ${BLUE}$kern${PLAIN}"
+echo -e "IP(s)                : ${BLUE}$ips${PLAIN}"
+echo -e "Last login           : ${BLUE}$last_logon${PLAIN}"
+next
+
